ec0ff420eb7658ab0fa3a896f4cca995af9fc092
Kingram <kingram@163.com>
2023-07-17 18:35:54 +0800

    close channel

--- channel/closechannel/closechannel.go
+++ channel/closechannel/closechannel.go
@@ -1,16 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	ch := make(chan struct{})
+	ch := make(chan int, 10)
 
+	var wg = sync.WaitGroup{}
+	wg.Add(1)
 	go func() {
-		defer close(ch)
-		ch <- struct{}{}
+		ch <- 1
+		ch <- 2
+		ch <- 3
+		ch <- 4
+		ch <- 5
+		close(ch)
+		wg.Done()
 	}()
 
 	// 已经关闭的channel是可以继续读取数据的，但是不能写入
-	a := <-ch
-	fmt.Println(a)
+	wg.Wait()
+	for i := 0; i < 7; i++ {
+		a := <-ch
+		fmt.Println(a)
+	}
+	ch <- 6 //panic
 }
